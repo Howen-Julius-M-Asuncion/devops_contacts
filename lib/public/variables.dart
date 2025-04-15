4fadb0e98676c5959d307cc34896cce2ebd935b2
Howen-Julius-M-Asuncion <kaiatmachines@gmail.com>
2025-04-16 05:24:19 +0800

    Update generated assets, remove commons

--- lib/public/variables.dart
+++ lib/public/variables.dart
@@ -1,5 +1,6 @@
 String selectedName = "";
 String selectedPhoto = "";
+bool selectedIsLocal = false;
 List<String> selectedPhone = [];
 List<String> selectedEmail = [];
 List<String> selectedUrl = [];
